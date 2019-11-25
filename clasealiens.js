93d90d892287c135e1fa2e150dacd0d3115563d5
Jose Miguel Sanchez <55396436+JosoByte@users.noreply.github.com>
2019-11-25 13:01:12 +0100

    Add files via upload

---
+++ clasealiens.js
@@ -0,0 +1,18 @@
+class juego(){
+	constructor(){
+		this.posAlien = new Array();
+		for (i=0;i<30;i++){
+			this.posAlien[i]=new Alien();
+			posAlien[i].x=posAlien[].length*50;
+		}
+	}	
+}
+class alien(){
+	constructor(){
+		var alien = patata;
+		this.x=0
+		this.y=0;
+		this.alive=true;
+		this.canShoot=true;
+	}	
+}
